c112667e6484cf5adbc49ae30598d36fd50e18e6
Eli <eli_tgt@hotmail.com>
2023-08-29 23:44:35 +0000

    feat(model): final league table

--- app/scripts/fct_matches.sql
+++ app/scripts/fct_matches.sql
@@ -9,14 +9,29 @@ data->>'match_round' as match_round,
 data->>'league_id' as league_id,
 upper(data->>'league_name') as league_name,
 data->>'league_year' as league_year,
-data->>'match_hometeam_score' as match_hometeam_score,
-upper(data->>'match_hometeam_name') as match_hometeam_name,
+cast(data->>'match_hometeam_score' as int) as match_hometeam_score,
+case
+when upper(data->>'match_hometeam_name') like '%TOTTENHAM%' then 'TOTTENHAM HOTSPUR'
+when upper(data->>'match_hometeam_name') like '%WEST HAM%' then 'WEST HAM UNITED'
+when upper(data->>'match_hometeam_name') like '%NEWCASTLE%' then 'NEWCASTLE UNITED'
+when upper(data->>'match_hometeam_name') like '%MANCHESTER U%' then 'MANCHESTER UNITED'
+else upper(data->>'match_hometeam_name')
+end as match_hometeam_name,
 data->>'match_hometeam_id' as match_hometeam_id,
-data->>'match_awayteam_score' as match_awayteam_score,
-upper(data->>'match_awayteam_name') as match_awayteam_name,
+cast(data->>'match_awayteam_score' as int) as match_awayteam_score,
+case
+when upper(data->>'match_awayteam_name') like '%TOTTENHAM%' then 'TOTTENHAM HOTSPUR'
+when upper(data->>'match_awayteam_name') like '%WEST HAM%' then 'WEST HAM UNITED'
+when upper(data->>'match_awayteam_name') like '%NEWCASTLE%' then 'NEWCASTLE UNITED'
+when upper(data->>'match_awayteam_name') like '%MANCHESTER U%' then 'MANCHESTER UNITED'
+else upper(data->>'match_awayteam_name')
+end as match_awayteam_name,
 data->>'match_awayteam_id' as match_awayteam_id,
 inserted_at
 from raw_football_data
+where data->>'league_year' = '2022/2023'
+and upper(data->>'league_name') = 'PREMIER LEAGUE'
+and upper(data->>'match_status') = 'FINISHED'
 ),
 results as (
 select
@@ -60,8 +75,5 @@ dedup as (
         ) AS row_version
   from points
 )
-select * from points
-where league_year = '2022/2023'
-and league_name = 'PREMIER LEAGUE'
-order by match_date
+select * from dedup where row_version = 1
 )
--- app/scripts/final_league_table.sql
+++ app/scripts/final_league_table.sql
@@ -1,13 +1,61 @@
+with home_result as (
 select
 match_hometeam_name as team_name,
 count(distinct(match_id)) as matches_played,
 sum(case when hometeam_result='WIN' then 1 else 0 end) as won,
 sum(case when hometeam_result='DRAW' then 1 else 0 end) as draw,
 sum(case when hometeam_result='LOSS' then 1 else 0 end) as lost,
-sum(match_hometeam_score) as goals_scored,
-sum(match_awayteam_score) as goals_conceded,
+sum(match_hometeam_score::int) as goals_scored,
+sum(match_awayteam_score::int) as goals_conceded,
 sum(hometeam_points) as points
 from 
 fct_matches
 group by
 team_name
+),
+away_result as (
+select
+match_awayteam_name as team_name,
+count(distinct(match_id)) as matches_played,
+sum(case when awayteam_result='WIN' then 1 else 0 end) as won,
+sum(case when awayteam_result='DRAW' then 1 else 0 end) as draw,
+sum(case when awayteam_result='LOSS' then 1 else 0 end) as lost,
+sum(match_awayteam_score::int) as goals_scored,
+sum(match_hometeam_score::int) as goals_conceded,
+sum(awayteam_points) as points
+from 
+fct_matches
+group by
+team_name
+),
+union_scores as (
+select
+*
+from
+home_result
+union all
+select
+*
+from
+away_result
+),
+final_result as (
+select
+rank () over (
+order by sum(points) desc,
+sum(goals_scored-goals_conceded) desc,
+sum(goals_scored) desc,
+sum(goals_conceded) asc, 
+sum(won) desc ) as position,
+team_name,
+sum(matches_played) as matches_played,
+sum(won) as won,
+sum(draw) as draw,
+sum(lost) as lost,
+sum(goals_scored) as goals_scored,
+sum(goals_conceded) as goals_conceded,
+sum(points) as points
+from union_scores
+group by team_name
+)
+select * from final_result order by position asc
